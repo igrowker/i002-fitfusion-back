17cb2c4c00a8cea00e35671c172c32e94bd0fc5c
LuisLiriano03 <llirianomendez@hotmail.com>
2024-06-29 00:34:28 +0200

    added changes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
-//Bienvenido a la api de FitFusion
-//El flujo va ser el siguente
-//1. En en el archivo router.js se crean las funciones que van a estar guardadas en la carpeta routes
-//Aclaracion! las rutas a igual que los handlers van a ir separadasen carpetas
-//con el nombre de la funcion o ruta que se quiera crear.
-// ej: routes/getIdUserRoute/getIdUserRoute.js
-// ej: handlers/getIdUser/getIdUser.js
+
+/*
+import './models/AboutUsDev.js'
+import './models/Class.js'
+import './models/Feedback.js'
+import './models/LevelClass.js'
+import './models/NutritionContent.js'
+import './models/Payment.js'
+import './models/Physiotherapist.js'
+import './models/Rol.js'
+import './models/StatusClass.js'
+import './models/Teacher.js'
+import './models/TypeClass.js'
+import './models/User.js'
+import './models/UserClass.js'*/
+
 
 const serverPort = process.env.serverPort;
 
@@ -14,13 +23,12 @@ async function main() {
   try {
     await sequelize.sync()
     console.log("Connection has been established successfully.");
+    app.listen(serverPort)
+    console.log("Server is listening on port " + serverPort);
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 
-  app.listen(serverPort, () => {
-    console.log("Hola mundo en el puerto " + serverPort);
-  });
 }
 
- main()
+main();
