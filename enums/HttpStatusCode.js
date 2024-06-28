75e2ceb790e1aaa4bf3bced553363fdb0d6a7c10
LuisLiriano03 <llirianomendez@hotmail.com>
2024-06-29 00:36:17 +0200

    added enums

---
+++ enums/HttpStatusCode.js
@@ -0,0 +1,6 @@
+const HttpStatusCode = {
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500
+};
+
+export default HttpStatusCode
---
+++ enums/RoleStatus.js
@@ -0,0 +1,5 @@
+const RoleStatus = {
+    NOT_FOUND: 0
+};
+
+export default RoleStatus;
