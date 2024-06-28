79e621726b1d9b03b6c25305595f4002bbe83725
LuisLiriano03 <llirianomendez@hotmail.com>
2024-06-29 00:35:38 +0200

    added db

--- database/database.js
+++ database/database.js
@@ -1,11 +1,16 @@
 import { Sequelize } from "sequelize";
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const user = process.env.user
 const password = process.env.password
 const host = process.env.host
-console.log(user, password, host)
 
-export const sequelize = new Sequelize('fitfusion', user, password, {
+export const sequelize = new Sequelize('dbfitfusion', user, password, {
     host: host,
     dialect: "mysql"
-  });
+});
+
+export default sequelize;
