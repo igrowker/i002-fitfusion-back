e772019571cab2eda82f787df09def3da88eafd3
LuisLiriano03 <llirianomendez@hotmail.com>
2024-07-01 11:16:02 +0200

    changes added so that name, email, password and role are not null and the others are

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../database/database.js';
 import Rol from './Rol.js';
-//
+
 const User = sequelize.define('User', {
   UserId: {
     type: DataTypes.INTEGER,
@@ -26,34 +26,39 @@ const User = sequelize.define('User', {
       model: Rol,
       key: 'RolId',
     },
-  },
-  Type: {
-    type: DataTypes.STRING(50),
     allowNull: false,
   },
   Age: {
     type: DataTypes.INTEGER,
+    allowNull: true, // Edad puede ser null
   },
   Residence: {
     type: DataTypes.STRING(100),
+    allowNull: true, // Residencia puede ser null
   },
   PhysicalCondition: {
     type: DataTypes.STRING(255),
+    allowNull: true, // Condición física puede ser null
   },
   ActivityLevel: {
     type: DataTypes.STRING(50),
+    allowNull: true, // Nivel de actividad puede ser null
   },
   Weight: {
     type: DataTypes.DECIMAL(5, 2),
+    allowNull: true, // Peso puede ser null
   },
   Height: {
     type: DataTypes.DECIMAL(5, 2),
+    allowNull: true, // Altura puede ser null
   },
   Goals: {
     type: DataTypes.TEXT,
+    allowNull: true, // Metas pueden ser null
   },
   Preferences: {
     type: DataTypes.TEXT,
+    allowNull: true, // Preferencias pueden ser null
   },
   IsActive: {
     type: DataTypes.BOOLEAN,
@@ -66,3 +71,4 @@ const User = sequelize.define('User', {
 User.belongsTo(Rol, { foreignKey: 'RolId' });
 
 export default User;
+
