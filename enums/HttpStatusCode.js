91da652802dbea9f0ea067e4e4a93c847f652067
LuisLiriano03 <llirianomendez@hotmail.com>
2024-07-01 11:20:51 +0200

    added new status

--- enums/HttpStatusCode.js
+++ enums/HttpStatusCode.js
@@ -1,6 +1,9 @@
 const HttpStatusCode = {
     NOT_FOUND: 404,
-    INTERNAL_SERVER_ERROR: 500
+    INTERNAL_SERVER_ERROR: 500,
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400
 };
 
 export default HttpStatusCode
