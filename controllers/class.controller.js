9f66a68163db04597565e7cda200149a90089aa4
Fer2313 <fernandojosefantini7.0@gmail.com>
2024-07-03 12:31:04 -0300

    correciones de codigo

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -46,8 +46,27 @@ export async function updateClassByID(req, res) {
         .status(HttpStatusCode.NOT_FOUND)
         .json({ message: "Class update Not Found" });
     } else {
-      const newClass = classd.update(classBody);
-      return res.status(HttpStatusCode.OK).body(newClass)
+      const newClass = await classd.update(classBody);
+      return res.status(HttpStatusCode.OK).json(newClass)
+    }
+  } catch (error) {
+    return res
+      .status(HttpStatusCode.INTERNAL_SERVER_ERROR)
+      .json({ message: "Internal server error" });
+  }
+}
+
+export async function deleteClassByID(req, res){
+  try {
+    const { id } = req.params;
+    const classd = await Class.findByPk(id);
+    if (!classd) {
+      return res
+        .status(HttpStatusCode.NOT_FOUND)
+        .json({ message: "Class update Not Found" });
+    }else{
+      await classd.destroy()
+      return res.status(HttpStatusCode.OK).json({status:"ok", message:"Class deleted"})
     }
   } catch (error) {
     return res
