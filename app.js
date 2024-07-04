724085dc90562bcaa650987dbc407a4cc66cfc0f
LuisLiriano03 <llirianomendez@hotmail.com>
2024-07-04 22:34:21 +0200

    added

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv"
 import rolsRoutes from './routes/rols.routes.js'
 import authRoutes from './routes/auth.routes.js'
 import usersRoutes from './routes/user.routes.js'
+import teacherRoutes from './routes/teacher.routes.js'
 import classesRoutes from './routes/class.routes.js'
+import statusClassRoutes from './routes/statusClass.routes.js'
+import typeClassRoutes from './routes/typeClass.routes.js'
+import levelClassRutes from './routes/levelClas.routes.js'
 
 dotenv.config();
 
@@ -16,6 +20,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/rol', rolsRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
-app.use('/api/classes', classesRoutes)
+app.use('/api/teacher', teacherRoutes);
+app.use('/api/classes', classesRoutes);
+app.use('/api/statusClass', statusClassRoutes);
+app.use('/api/typeClass', typeClassRoutes);
+app.use('/api/levelClass', levelClassRutes);
 
 export default app;
