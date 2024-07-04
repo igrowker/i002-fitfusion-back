680aedc9634b1466427f2eb752b1ca9198076db4
LuisLiriano03 <llirianomendez@hotmail.com>
2024-07-04 22:31:01 +0200

    fix

---
+++ enums/DataNoFound.js
@@ -0,0 +1,5 @@
+const DataNoFound = {
+    NOT_FOUND: 0
+};
+
+export default DataNoFound;
--- enums/HttpStatusCode.js
+++ enums/HttpStatusCode.js
@@ -3,7 +3,8 @@ const HttpStatusCode = {
     INTERNAL_SERVER_ERROR: 500,
     OK: 200,
     CREATED: 201,
-    BAD_REQUEST: 400
+    BAD_REQUEST: 400,
+    CONFLICT: 409
 };
 
 export default HttpStatusCode
