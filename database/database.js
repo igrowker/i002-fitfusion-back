f6fbcd7df90791e16173c305fe51053416c71570
nmacenco <nicolasmacenco@gmail.com>
2024-07-10 16:41:49 +0200

    feat: Se agregan las configuraciones para conectar con la base de datos en la nube (filess.io)

--- database/database.js
+++ database/database.js
@@ -1,18 +1,19 @@
 import { Sequelize } from "sequelize";
 
-import dotenv from 'dotenv';
-
-
+import dotenv from "dotenv";
 
 dotenv.config();
 
-const user = process.env.user
-const password = process.env.password
-const host = process.env.host
+const user = process.env.user;
+const password = process.env.password;
+const host = process.env.host;
+const port = process.env.port;
+const database = process.env.database;
 
-export const sequelize = new Sequelize('dbfitfusion', user, password, {
-    host: host,
-    dialect: "mysql"
+export const sequelize = new Sequelize( database , user, password, {
+  host: host,
+  port: port,
+  dialect: "mysql",
 });
 
 export default sequelize;
