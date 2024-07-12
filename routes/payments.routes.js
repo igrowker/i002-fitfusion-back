1f28a4904038a623cbf4d85a7f041828c136cd4c
sbalbarrey <santiagobalbarrey@gmail.com>
2024-07-12 13:18:36 -0300

    payment stripe

--- routes/payments.routes.js
+++ routes/payments.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createPayment, getPaymentsUser } from "../controllers/payments.controller.js";
+import { createPayment, getPaymentsUser, createPaymentStripe } from "../controllers/payments.controller.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-// router.post('/', authMiddleware, createPayment);
-router.post('/',  createPayment);
+router.post('/', authMiddleware, createPayment);
+router.post('/stripe', authMiddleware, createPaymentStripe);
 router.get('/:id', authMiddleware, getPaymentsUser);
 
 export default router;
